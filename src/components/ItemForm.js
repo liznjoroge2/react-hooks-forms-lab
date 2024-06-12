393c3d23d7c37ad69e3ac1b9e2190c45d2ceec2a
enjoroge <njrogeelizabeth53@gmail.com>
2024-06-12 21:52:08 +0300

    commit

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,17 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import { v4 as uuid } from "uuid";
 
-function ItemForm(props) {
+function ItemForm({ onItemFormSubmit }) {
+  const [itemName, setItemName] = useState("");
+  const [itemCategory, setItemCategory] = useState("Produce");
+
+  // Function to update item name state
+  function handleNameChange(e) {
+    setItemName(e.target.value);
+  }
+  
+  // Function to update item category state
+  function handleCategoryChange(e) {
+    setItemCategory(e.target.value);
+  }
+
+  // Function to handle form submission
+  function handleSubmit(event) {
+    event.preventDefault();
+    const newItem = {
+      id: uuid(),
+      name: itemName,
+      category: itemCategory,
+    };
+    onItemFormSubmit(newItem);
+    setItemName("");
+    setItemCategory("Produce");
+  }
+
   return (
-    <form className="NewItem">
+    <form className="NewItem" onSubmit={handleSubmit}>
       <label>
         Name:
-        <input type="text" name="name" />
+        <input
+          type="text"
+          name="name"
+          value={itemName}
+          onChange={handleNameChange} // Use the function directly
+        />
       </label>
 
       <label>
         Category:
-        <select name="category">
+        <select
+          name="category"
+          value={itemCategory}
+          onChange={handleCategoryChange} // Use the function directly
+        >
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
